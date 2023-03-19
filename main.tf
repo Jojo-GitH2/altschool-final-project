a519941f78b895c77d1bb9ef086e738ffc8b5735
Jojo-GitH2 <jonah.uka@gmail.com>
2023-03-19 23:08:09 +0000

    Add files

---
+++ main.tf
@@ -0,0 +1,47 @@
+//a Kubernetes cluster
+
+# instance kubectl linkto the cluster run the kubernetes
+
+resource "digitalocean_kubernetes_cluster" "kubernetes_cluster" {
+  version = var.k8s_version
+  name    = var.k8s_clustername
+  # label = var.label
+  region = var.region
+  tags   = var.tags
+
+  node_pool {
+    name = var.pools.0.name
+    node_count = var.pools.0.count
+    size       = var.pools.0.size
+    }
+  }
+
+
+//Export this cluster's attributes
+
+output "k8s_name" {
+  value = digitalocean_kubernetes_cluster.kubernetes_cluster.name
+}
+
+
+output "kubeconfig" {
+  value     = digitalocean_kubernetes_cluster.kubernetes_cluster.kube_config.0.raw_config
+  sensitive = true
+}
+
+output "api_endpoints" {
+  value = digitalocean_kubernetes_cluster.kubernetes_cluster.endpoint
+}
+
+output "status" {
+  value = digitalocean_kubernetes_cluster.kubernetes_cluster.status
+}
+
+output "id" {
+  value = digitalocean_kubernetes_cluster.kubernetes_cluster.id
+}
+
+output "pool" {
+  value = digitalocean_kubernetes_cluster.kubernetes_cluster.node_pool
+}
+
