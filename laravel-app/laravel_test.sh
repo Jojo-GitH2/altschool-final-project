484ff36db69976ca565b9225339060e2dccf5725
Jojo-GitH2 <jonah.uka@gmail.com>
2023-03-20 01:00:29 +0000

    Update laravel_test

--- laravel-app/laravel_test.sh
+++ laravel-app/laravel_test.sh
@@ -1,22 +1,22 @@
 export DEBIAN_FRONTEND=noninteractive
 
-apt update \
-    && apt install nginx wget lsb-release software-properties-common -y
+sudo apt-get update \
+    && sudo apt-get install nginx wget lsb-release software-properties-common -y
 
-cp -r ./laravel-realworld-example-app /var/www/html/laravel-realworld-example-app
+sudo cp -r ./laravel-realworld-example-app /var/www/html/laravel-realworld-example-app
 
-cp ./.env.example /var/www/html/laravel-realworld-example-app/.env
+sudo cp ./.env.example /var/www/html/laravel-realworld-example-app/.env
 
-apt install apt-transport-https gnupg2 ca-certificates -y
+sudo apt-get install apt-transport-https gnupg2 ca-certificates -y
 
-apt install php8.1-fpm libapache2-mod-php php php-common php-xml php-gd php8.1-opcache php-mbstring php-tokenizer \
+sudo apt-get install php8.1-fpm libapache2-mod-php php php-common php-xml php-gd php8.1-opcache php-mbstring php-tokenizer \
     php-json php-bcmath php-zip unzip curl php8.1-curl php8.1-mbstring php8.1-xml zip php-mysql php8.1-mysql vim git -y
 
-cp ./php.ini /etc/php/8.1/fpm/php.ini
+sudo cp ./php.ini /etc/php/8.1/fpm/php.ini
 
-cp ./laravel /etc/nginx/sites-available/laravel
+sudo cp ./laravel /etc/nginx/sites-available/laravel
 
-cp ./web.php /var/www/html/laravel-realworld-example-app/routes/web.php
+sudo cp ./web.php /var/www/html/laravel-realworld-example-app/routes/web.php
 
 cd /var/www/html/
 
@@ -34,9 +34,9 @@ composer create-project --no-interaction
 
 ln -s /etc/nginx/sites-available/laravel /etc/nginx/sites-enabled/
 
-rm /etc/nginx/sites-enabled/default
+sudo rm /etc/nginx/sites-enabled/default
 
-service nginx reload && service php8.1-fpm restart
+sudo service nginx reload && sudo service php8.1-fpm restart
 
 php artisan migrate --seed
 
