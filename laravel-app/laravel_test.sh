c42d6ffd8f904ed8447d4d41835ceedd4d0aa00b
Jojo-GitH2 <jonah.uka@gmail.com>
2023-03-20 01:51:21 +0000

    Update laravel_test

--- laravel-app/laravel_test.sh
+++ laravel-app/laravel_test.sh
@@ -2,6 +2,7 @@ export DEBIAN_FRONTEND=noninteractive
 
 sudo apt-get update \
     && sudo apt-get install nginx wget lsb-release software-properties-common -y
+sudo systemctl enable nginx
 
 sudo cp -r ./laravel-realworld-example-app /var/www/html/laravel-realworld-example-app
 
@@ -9,8 +10,12 @@ sudo cp ./.env.example /var/www/html/laravel-realworld-example-app/.env
 
 sudo apt-get install apt-transport-https gnupg2 ca-certificates -y
 
-sudo apt-get install php8.1-fpm libapache2-mod-php php php-common php-xml php-gd php8.1-opcache php-mbstring php-tokenizer \
-    php-json php-bcmath php-zip unzip curl php8.1-curl php8.1-mbstring php8.1-xml zip php-mysql php8.1-mysql vim git -y
+wget -qO - https://packages.sury.org/php/apt.gpg | sudo apt-key add -
+
+sudo apt-get update && sudo apt-get upgrade -y
+
+sudo apt-get install php8.1-fpm libapache2-mod-php php8.1 php-common php-xml php-gd php8.1-opcache php-mbstring php-tokenizer \
+    php-json php-bcmath php-zip unzip curl php8.1-curl php8.1-mbstring php8.1-xml zip php-mysql php8.1-mysql vim git gnupg gnupg2 -y
 
 sudo cp ./php.ini /etc/php/8.1/fpm/php.ini
 
@@ -29,10 +34,10 @@ curl -sS https://getcomposer.org/installer | php \
 sudo update-alternatives --set php /usr/bin/php8.1
 
 cd /var/www/html/laravel-realworld-example-app/
-
-sudo composer create-project --no-interaction
-
-sudo ln -s /etc/nginx/sites-available/laravel /etc/nginx/sites-enabled/
+php artisan key:generate
+#sudo composer create-project --no-interaction
+composer install
+sudo ln -s /etc/nginx/sites-available/laravel /etc/nginx/sites-enabled/laravel
 
 sudo rm /etc/nginx/sites-enabled/default
 
