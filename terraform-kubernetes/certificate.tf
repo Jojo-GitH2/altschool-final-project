67a3a62ef359d59c9083686e7cfeea0a0ba7979c
Jonah Uka <jonah.uka@gmail.com>
2023-03-24 02:17:10 +0000

    Update files

--- terraform-kubernetes/certificate.tf
+++ terraform-kubernetes/certificate.tf
@@ -1,33 +1,33 @@
 # resource "aws_acm_certificate" "sock-shop" {
-#     domain_name = "sock-shop.altschool-demo.me"
-#     validation_method = "DNS"
+#   domain_name       = "sock-shop.altschool-demo.me"
+#   validation_method = "DNS"
 # }
 
 # data "aws_route53_zone" "sock-shop-kubernetes" {
-#   name = "sock-shop.altschool-demo.me"
+#   name         = "sock-shop.altschool-demo.me"
 #   private_zone = false
-  
+
 # }
 
 # resource "aws_route53_record" "api_validation" {
 #   for_each = {
-#     for dvo in aws_acm_certificate.sock-shop.domain_validation_options : dvo.domain_name =>  {
-#       name = dvo.resource_record_name
+#     for dvo in aws_acm_certificate.sock-shop.domain_validation_options : dvo.domain_name => {
+#       name   = dvo.resource_record_name
 #       record = dvo.resource_record_value
-#       type = dvo.resource_record_type
+#       type   = dvo.resource_record_type
 #     }
 #   }
 #   allow_overwrite = true
-#   name = each.value.name
-#   records = [each.value.record]
-#   ttl = 60
-#   type = each.value.type
-#   zone_id = data.aws_route53_zone.sock-shop-kubernetes.zone_id
+#   name            = each.value.name
+#   records         = [each.value.record]
+#   ttl             = 60
+#   type            = each.value.type
+#   zone_id         = data.aws_route53_zone.sock-shop-kubernetes.zone_id
 # }
 
 # resource "aws_acm_certificate_validation" "sock-shop" {
 #   certificate_arn = aws_acm_certificate.sock-shop.arn
 #   validation_record_fqdns = [
 #     for record in aws_route53_record.api_validation : record.fqdn
-#     ]
+#   ]
 # } 
--- terraform-kubernetes/route53.tf
+++ terraform-kubernetes/route53.tf
@@ -2,6 +2,6 @@
 # data "aws_elb_hosted_zone_id" "elb_zone_id" {
 #   name = "sock-shop.altschool-demo.me"
 #   depends_on = [
-  
+
 #   ]
 # }
