107957bb27486a029a01fbc03d0c422b5324501b
Jonah Uka <jonah.uka@gmail.com>
2023-03-27 15:43:15 +0000

    Update

--- terraform-domain/load-balancer.tf
+++ terraform-domain/load-balancer.tf
@@ -4,6 +4,6 @@ data "aws_lb" "sock-shop-loadbalancer" {
 }
 
 data "aws_lb" "voting-app-loadbalancer" {
-  # arn = data.aws_kms_secrets.decrypt_arn.plaintext["voting-app-arn"]
+  arn = data.aws_kms_secrets.decrypt_arn.plaintext["voting-app-arn"]
   # arn = var.encrypted-voting-app-loadbalancer
 }
