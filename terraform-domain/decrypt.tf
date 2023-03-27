3ca32227962ba98c6f302706a812529ff37d64db
Jonah Uka <jonah.uka@gmail.com>
2023-03-27 15:40:44 +0000

    Update

--- terraform-domain/decrypt.tf
+++ terraform-domain/decrypt.tf
@@ -1,19 +1,19 @@
-# data "aws_kms_key" "altschool-kubernetes" {
-#   key_id = "alias/eks/altschool-kubernetes"
-# }
+data "aws_kms_key" "altschool-kubernetes" {
+  key_id = "alias/eks/altschool-kubernetes"
+}
 
-# data "aws_kms_secrets" "decrypt_arn" {
-#   secret {
-#     name                 = "sock-shop-arn"
-#     payload              = var.encrypted-sock-shop-loadbalancer
-#     # encryption_algorithm = "SYMMETRIC_DEFAULT"
-#     # key_id               = data.aws_kms_key.altschool-kubernetes.key_id
-#   }
+data "aws_kms_secrets" "decrypt_arn" {
+  secret {
+    name    = "sock-shop-arn"
+    payload = var.encrypted-sock-shop-loadbalancer
+    # encryption_algorithm = "SYMMETRIC_DEFAULT"
+    # key_id               = data.aws_kms_key.altschool-kubernetes.key_id
+  }
 
-#   secret {
-#     name                 = "voting-app-arn"
-#     payload              = var.encrypted-voting-app-loadbalancer
-#     # encryption_algorithm = "SYMMETRIC_DEFAULT"
-#     # key_id               = data.aws_kms_key.altschool-kubernetes.key_id
-#   }
-# }
+  secret {
+    name    = "voting-app-arn"
+    payload = var.encrypted-voting-app-loadbalancer
+    # encryption_algorithm = "SYMMETRIC_DEFAULT"
+    # key_id               = data.aws_kms_key.altschool-kubernetes.key_id
+  }
+}
