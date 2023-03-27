4f176f0aede8ca6213ca4eaa10e7ff2c3d8e1038
Jonah Uka <jonah.uka@gmail.com>
2023-03-27 15:05:12 +0000

    Update files

--- terraform-domain/decrypt.tf
+++ terraform-domain/decrypt.tf
@@ -6,14 +6,14 @@ data "aws_kms_secrets" "decrypt_arn" {
   secret {
     name                 = "sock-shop-arn"
     payload              = var.encrypted-sock-shop-loadbalancer
-    encryption_algorithm = "SYMMETRIC_DEFAULT"
-    key_id               = data.aws_kms_key.altschool-kubernetes.key_id
+    # encryption_algorithm = "SYMMETRIC_DEFAULT"
+    # key_id               = data.aws_kms_key.altschool-kubernetes.key_id
   }
 
   secret {
     name                 = "voting-app-arn"
     payload              = var.encrypted-voting-app-loadbalancer
-    encryption_algorithm = "SYMMETRIC_DEFAULT"
-    key_id               = data.aws_kms_key.altschool-kubernetes.key_id
+    # encryption_algorithm = "SYMMETRIC_DEFAULT"
+    # key_id               = data.aws_kms_key.altschool-kubernetes.key_id
   }
 }
